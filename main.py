96309b9ee2f6780be927b4c20217426bfd12c7b4
KosBar49 <34551054+KosBar49@users.noreply.github.com>
2022-10-21 00:43:15 +0200

    init commit

---
+++ main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+if __name__ == "__main__":
+  #zadeklaruj zmienną typu int
+  a = 7
+  
+  #zadeklaruj zmienną typu float
+  
+  #zadeklaruj zmienną typu bool
+  
+  #zadeklaruj zmienną typu complex
+  
+  #zadeklaruj zmienna typue string
+  
+  
+  #wyświetl typy zadeklarowanych zmiennych
+  print('zmienna a jest typu: ', type(a))
+  
+  #wykonaj dzialanie potęgowania na dowolnej zmiennej 
+  
+  #wykonaj działanie mnożenie na dowolnej zmiennej
+  
+  #wykonaj działanie dodawania na zmiennej typu float i int
+  
+  #sprawdź co zwróci operacja mnożenia zmiennej typu float i string
+  
+  
+  
+  
